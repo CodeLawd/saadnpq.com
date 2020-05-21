fc2b6f4c25a8628ead7831498bcfd6f29b2c6782
saadnpq <saadnpq@localhost.localdomain>
2020-05-21 10:57:07 +0200

    update project section

--- src/pages/home-sections/Projects.js
+++ src/pages/home-sections/Projects.js
@@ -32,7 +32,7 @@ function Projects() {
     <Center id="projects">
       <Title >Featured Projects</Title>
       <AboutProjects>
-        When i decide to go for a project idea, i don't start from a skill i want to demonstrate, instead, i start from a real problem i think i can solve or a gap i think i can fill. So my active knowledge can't be demonstrated fully by the projects i have done so far.
+        When i decide to go for a project idea, i don't start from "oh look what i can do, lets make something out of it", instead, i start from a real problem i think i can solve or a gap i think i can fill. So my active knowledge can't be demonstrated fully by the projects i have done so far.
       </AboutProjects>
       <ProjectListingContainer>
         <ProjectListing selected={true}/>
